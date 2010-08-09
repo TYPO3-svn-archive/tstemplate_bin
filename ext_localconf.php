2ce45b57ce09b5a552683937aa5d86427efc28a3
metti <metti@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-09 21:08:15 +0000

    * Add conf template to set path and prefix * Fixed wrong paths for hooks
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tstemplate_bin/trunk/tstemplate_bin@36552 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 // Define hooks:
-$class = 'EXT:t3editor_bin/class.tx_tstemplatebin.php:&tx_tstemplatebin';
+$class = 'EXT:tstemplate_bin/class.tx_tstemplatebin.php:&tx_tstemplatebin';
 $TYPO3_CONF_VARS['SC_OPTIONS']['ext/tstemplate_info/class.tx_tstemplateinfo.php']['postOutputProcessingHook'][] = $class.'->postOutputProcessingHook';
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = $class;
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getMainFieldsClass'][] = $class;
