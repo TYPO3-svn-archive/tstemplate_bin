687e168a8f825ab29fbf0af37be5fc4c114a177d
metti <metti@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-10 06:15:57 +0000

    * Comment-Template from extconf * Adding comments to the textarea if in template module (info/modify) * Nicer conf descriptions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tstemplate_bin/trunk/tstemplate_bin@36554 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,4 +6,14 @@ $class = 'EXT:tstemplate_bin/class.tx_tstemplatebin.php:&tx_tstemplatebin';
 $TYPO3_CONF_VARS['SC_OPTIONS']['ext/tstemplate_info/class.tx_tstemplateinfo.php']['postOutputProcessingHook'][] = $class.'->postOutputProcessingHook';
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = $class;
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getMainFieldsClass'][] = $class;
+
+$TYPO3_CONF_VARS['EXTCONF']['tstemplate_bin']['fileComment'] = 
+'/**
+ * Template ${title} (${uid}) in ${rootLine} (${pid})
+ * Included from ${templateFile}
+ * 
+ * @version $Id$
+ */
+
+';
 ?>
